d3f1ccb9c4b12e52fd0f5a6c6b8087627c3fd6a4
Haroldo <haroldobeyerbacellar@gmail.com>
2020-11-24 06:42:14 -0300

    Finishing monolith

--- monolith/index.ts
+++ monolith/index.ts
@@ -1,9 +1,10 @@
 import express, { json } from 'express';
 const app = express();
+app.use(json());
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const port = Math.floor(Math.random() * 10000);
-
+// const port = Math.floor(Math.random() * 10000);
+const port = 8358;
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
@@ -13,17 +14,19 @@ db.defaults({ posts: [], count: 0 }).write();
 
 
 app.get('/', (req, res) => {
+    const posts: Post[] = db.get('posts').map('title').value();
+
     return res.json({
-        message: `Posts: ${JSON.stringify(db.get('post').find().value())}`,
+        message: `Posts: ${posts}`,
         code: 200
     })
 });
 
 app.post('/', (req, res) => {
     const body = req.body;
-    console.log(JSON.stringify(body));
+    const title = body.title;
 
-    db.get('posts').push({ id: (Math.random() * 1000), title: body.title }).write();
+    db.get('posts').push({ id: (Math.random() * 1000), title }).write();
     db.update('count', (n: number) => n++).write();
     res.send("sent post request");
 });
@@ -31,3 +34,13 @@ app.post('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Running on ${port}`);
 });
+
+
+class Post {
+    id: string;
+    title: string;
+    constructor(id: string, title: string) {
+        this.id = id;
+        this.title = title;
+    }
+}

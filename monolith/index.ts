2dd3554608ed73164f439b0b27b1b3b5002d13b3
Haroldo <haroldobeyerbacellar@gmail.com>
2020-11-24 07:05:46 -0300

    testing init with monolith

--- monolith/index.ts
+++ monolith/index.ts
@@ -1,38 +1,52 @@
+let init = Date.now();
 import express, { json } from 'express';
 const app = express();
 app.use(json());
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-// const port = Math.floor(Math.random() * 10000);
-const port = 8358;
+const port = Math.floor(Math.random() * 10000);
+// const port = 8358;
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
 //padrão
-db.defaults({ posts: [], count: 0 }).write();
+db.defaults({ posts: [], count: 0, init: [], exec: [] }).write();
 
 
 
 app.get('/', (req, res) => {
+    init = Date.now();
     const posts: Post[] = db.get('posts').map('title').value();
 
-    return res.json({
+    const response = res.json({
         message: `Posts: ${posts}`,
         code: 200
     })
+
+    db.get("exec").push({
+        type: "get",
+        time: Date.now() - init
+    }).write()
+
+    return response;
 });
 
 app.post('/', (req, res) => {
+    init = Date.now();
     const body = req.body;
     const title = body.title;
-
     db.get('posts').push({ id: (Math.random() * 1000), title }).write();
     db.update('count', (n: number) => n++).write();
+    db.get("exec").push({
+        type: "post",
+        time: Date.now() - init
+    }).write();
     res.send("sent post request");
 });
 
 app.listen(port, () => {
     console.log(`Running on ${port}`);
+    db.get('init').push(Date.now() - init).write();
 });
 
 
